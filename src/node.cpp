3d007b475d4faba5a9da1eac62be3dd1c1f704c2
Rahul Shelke <srahul07@gmail.com>
2020-05-20 20:44:02 +0530

    Adding KeccakBuffer class changes

--- src/node.cpp
+++ src/node.cpp
@@ -2,31 +2,29 @@
 
 #include "utils/hex.hpp"
 #include "rlp/inc/rlpencode.hpp"
-#include "keccak/keccak.h"
+#include "keccak/keccak_buffer.hpp"
 
 
-std::vector<uint64_t> Node::GetValue() {
+buffer_t Node::GetValue() {
     return value_;
 }
 
-void Node::SetValue(std::vector<uint64_t> input) {
+void Node::SetValue(buffer_t input) {
     value_ = input;
 }
 
-std::vector<std::vector<uint64_t>> Node::Raw() {
-    std::vector<std::vector<uint64_t>> raw_;
+bufferarray_t Node::Raw() {
+    bufferarray_t raw_;
     raw_.resize(1, value_);
 
     return raw_;
 }
 
-std::vector<uint64_t> Node::Serialize() {
+buffer_t Node::Serialize() {
     return RLPEncoder::EncodeByteList(Raw());
 }
 
-std::vector<uint64_t> Node::Hash() {
-    Keccak keccak256 = Keccak(Keccak::Bits::Keccak256);
-    std::string byte_str_ = BytesToString(Serialize());
-    byte_str_ = keccak256(byte_str_);
-    return StringToBytes(byte_str_);
+buffer_t Node::Hash() {
+    KeccakBuffer buffer_hash_ = KeccakBuffer();
+    return buffer_hash_(Serialize());
 }
