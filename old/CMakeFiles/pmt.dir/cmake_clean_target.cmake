c86f5a6acc12513d8a8fa64f57791094e6d6cd52
Rahul Shelke <srahul07@gmail.com>
2020-06-17 17:37:40 +0530

    Deleting old code

--- old/CMakeFiles/pmt.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libpmt.a"
-)
--- old/src/core/constants.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PMT_CONSTANTS_H
-#define PMT_CONSTANTS_H
-
-#define EMPTY_STRING 0x00
-
-#define DEFAULT_DB_FILE "/tmp/pmt.db"
-
-#endif
